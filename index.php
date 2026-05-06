7252a134ed87a8a134e913835a5a3311d3841bcf
Firda12345 <mustafida1310@gmail.com>
2026-05-06 14:43:34 +0800

    T4-week10

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>T4-week10 - Daftar Inventaris</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body class="container mt-5">
     <div class="d-flex justify-content-between align-items-center mb-4">
